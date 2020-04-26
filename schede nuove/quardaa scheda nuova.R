af428c46ca7dfdebb86ec21742121ca77e4a2d28
Anna Niewczas <anna.nwcz@gmail.com>
2020-04-26 12:17:24 +0200

    provo a fare lo stesso commit su entrambe vediamo se cambia qualcosa

---
+++ schede nuove/quardaa scheda nuova.R
@@ -0,0 +1,3 @@
+facciamo una scheda nuova ancora.
+questa è la quarta per vedere se il commit si può fare su entrambi
+e se una volta su github il commit compare uguale
---
+++ schede nuove/schede nuove.R
@@ -0,0 +1,2 @@
+prova a fare un'altra scheda nuova 
+per vedere se entra nella cartella o se sta fuori
